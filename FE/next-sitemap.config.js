87789d7349b1abd363592471ee749e88ed097f8e
lamtupro <admin@quoclamtu.live>
2025-04-21 22:31:13 +0700

    add seo

--- FE/next-sitemap.config.js
+++ FE/next-sitemap.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next-sitemap').IConfig} */
+// next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://quoclamtu.live',
-    generateRobotsTxt: true, // Tự tạo robots.txt
-    changefreq: 'weekly', // Tần suất cập nhật
-    priority: 0.7,         // Độ ưu tiên các trang
-    sitemapSize: 5000,     // Tối đa số URL mỗi file sitemap
-  }
-  
+  siteUrl: 'https://quoclamtu.live',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/search'],
+}
