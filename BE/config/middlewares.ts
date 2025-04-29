2d5f611edc0ed4f1abf687b05cf5b833452075ec
lamtupro <admin@quoclamtu.live>
2025-04-30 01:10:03 +0700

    fix BE

--- BE/config/middlewares.ts
+++ BE/config/middlewares.ts
@@ -2,20 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: [
-        'https://quoclamtu.live', 
-        'https://www.quoclamtu.live',
-        'http://localhost:3000'
-      ], // 🔥 Chỉ cho phép domain này
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-      headers: ['Content-Type', 'Authorization'],
-      credentials: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
